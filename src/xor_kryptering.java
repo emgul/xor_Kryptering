c974cdb7249aafd41e430307bb52a3ca7110ed55
emgul <te14.emil.henriksson@umea.nti.se>
2017-04-28 09:33:02 +0200

    Add files via upload

--- src/xor_kryptering.java
+++ src/xor_kryptering.java
@@ -3,45 +3,48 @@ import java.util.*;
 
 public class xor_kryptering {
 
-    public static void main(String[] args) throws IOException {
+        public static void main(String[] args) throws IOException {
 
-        int place = -1;
+            String fil = "minfil.txt";
 
-        String fil = "minfil.txt";
+            FileReader fr = new FileReader(fil);
+            BufferedReader infil = new BufferedReader(fr);
 
-        FileReader fr = new FileReader(fil);
-        BufferedReader infil = new BufferedReader(fr);
+            String rad = infil.readLine();
+            System.out.println("Okrypterat = " + rad);
+            infil.close();
 
-        String rad = infil.readLine();
-        System.out.println("Okrypterat = " + rad);
-        infil.close();
+            StringBuilder sb = new StringBuilder();
+            Random r = new Random();
+            String alphabet = "abcdefghijklmnopqrstuvxyz";
+            for (int i = 0; i < rad.length(); i++) {
+                //System.out.print(alphabet.charAt(r.nextInt(alphabet.length())));
+                sb.append(alphabet.charAt(r.nextInt(alphabet.length())));
+            }
 
-        StringBuilder sb = new StringBuilder();
-        Random r = new Random();
-        String alphabet = "abcdefghijklmopqrstuvwxyz";
-        for (int i = 0; i < rad.length(); i++) {
-            //System.out.print(alphabet.charAt(r.nextInt(alphabet.length())));
-            sb.append(alphabet.charAt(r.nextInt(alphabet.length())));
+            String result = sb.toString();
+            System.out.println("nyckel = " + result);
+
+            File newTextFile = new File("nyckel.txt");
+            FileWriter fw = new FileWriter(newTextFile);
+            fw.write(result);
+            fw.close();
+
+            String crypt = "";
+            for (int i = 0; i < rad.length(); i++) {
+                char xorchar = (char) (rad.charAt(i) ^ result.charAt(i));
+                crypt += xorchar;
+            }
+            System.out.println("krypterad text = " + crypt);
+
+            String decrypt = "";
+            for (int i = 0; i < crypt.length(); i++) {
+                char decrypted = (char) (crypt.charAt(i) ^ result.charAt(i));
+                decrypt += decrypted;
+            }
+            System.out.println("dekrypterad text = " + decrypt);
+
+
         }
-        String result = sb.toString();
-        System.out.println("nyckel = " + result);
-
-        File newTextFile = new File("nyckel.txt");
-        FileWriter fw = new FileWriter(newTextFile);
-        fw.write(result);
-        fw.close();
-
-        StringBuilder builder = new StringBuilder();
-
-        for (int i = 0; i < rad.length(); i++) {
-            char xorchar = (char) (rad.charAt(i) ^ result.charAt(i));
-            System.out.print(xorchar);
-            
-        }
-
-
-
 
     }
-
-}
